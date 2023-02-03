7dd6958de86df39978041e112cfc7cd8b7ffb2a2
Farkhodoff19 <farkhodoffcodes@gmail.com>
2023-02-03 21:50:13 +0300

    advanced objects

--- 7-dars/script.js
+++ 7-dars/script.js
@@ -1,1 +1,33 @@
-// Objects Part II / Advanced Objects
+// // Objects Part II / Advanced Objects
+
+// let persons = {
+//   name: "Farkhodoff",
+//   age: 19,
+//   child: {
+//     name: "Najmiddin",
+//     age: 21,
+//     child: {
+//       name: "Botir",
+//       age: 22,
+//     },
+//   },
+// };
+
+// let sum = 0;
+
+// while (persons) {
+//   sum = sum + persons.age;
+
+//   persons = persons.child;
+// }
+
+// console.log(sum);
+
+let user = {
+  name: "Farkhodoff",
+  sayHi() {
+    console.log(`Hi ${this.name}`);
+  },
+};
+
+user.sayHi();
