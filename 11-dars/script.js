2fa676a4b7515400b5bec02987cffc1fa81e8c24
Farkhodoff19 <farkhodoffcodes@gmail.com>
2023-02-08 19:52:34 +0300

    write example one row code

--- 11-dars/script.js
+++ 11-dars/script.js
@@ -1,1 +1,12 @@
 // ADVANCED MASSIV / ARRAY/ TO"PLAM
+
+let arr = [
+  { id: 1, year: 2003, name: "Farhodov Og'abek" },
+  { id: 1, year: 2002, name: "Farhodov Og'abek" },
+  { id: 1, year: 2004, name: "Farhodov Og'abek" },
+  { id: 1, year: 2005, name: "Farhodov Og'abek" },
+];
+
+let response = arr.filter((value) => value.year === 2003);
+
+console.log(response);
