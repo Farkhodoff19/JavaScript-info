3d8c0314b083ffdbf5c4dca9685943c1b73e8c43
Farkhodoff19 <farkhodoffcodes@gmail.com>
2023-02-09 17:13:21 +0300

    advanced arrays

--- 11-dars/script.js
+++ 11-dars/script.js
@@ -1,12 +1,21 @@
-// ADVANCED MASSIV / ARRAY/ TO"PLAM
+// // ADVANCED MASSIV / ARRAY/ TO"PLAM
 
-let arr = [
-  { id: 1, year: 2003, name: "Farhodov Og'abek" },
-  { id: 1, year: 2002, name: "Farhodov Og'abek" },
-  { id: 1, year: 2004, name: "Farhodov Og'abek" },
-  { id: 1, year: 2005, name: "Farhodov Og'abek" },
-];
+// let arr = [
+//   { id: 1, year: 2003, name: "Farhodov Og'abek" },
+//   { id: 1, year: 2002, name: "Farhodov Og'abek" },
+//   { id: 1, year: 2004, name: "Farhodov Og'abek" },
+//   { id: 1, year: 2005, name: "Farhodov Og'abek" },
+// ];
 
-// let response = arr.filter((value) => value.year === 2003);
+// // let response = arr.filter((value) => value.year === 2003);
 
-// console.log(response);
+// // console.log(response);
+
+// let calc = 23 * "yigirma uch";
+
+// console.log(calc);
+
+let arrNumber = [23, 24, 26, 27, "19"];
+
+// console.log(arrNumber.some((value) => typeof value === "number"));
+console.log(arrNumber.fill("farkhodoff", 1));
