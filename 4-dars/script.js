6be4c355ff61d85744ff5e9aa77b61749e79df42
Farkhodoff19 <farkhodoffcodes@gmail.com>
2023-02-13 15:28:45 +0300

    advanced objects

--- 4-dars/script.js
+++ 4-dars/script.js
@@ -34,4 +34,12 @@
 //   if (i % 2 === 0) {
 //     console.log(`This number juf ${i}`);
 //   }
+//
+
+// for (let i = 1; i < 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(`${i} This is not Prime number `);
+//   } else {
+//     console.log(`${i} This is Prime number`);
+//   }
 // }
