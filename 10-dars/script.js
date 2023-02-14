b0639193923dd932f92a061b72595798f8084d5a
Farkhodoff19 <farkhodoffcodes@gmail.com>
2023-02-14 14:49:34 +0300

    commited all code

--- 10-dars/script.js
+++ 10-dars/script.js
@@ -1,1 +1,20 @@
 // MASSIV / ARRAY/ TO"PLAM
+// let arr = [
+//   {},
+//   "farkhodoff",
+//   19,
+//   true,
+//   false,
+//   [],
+//   function get() {},
+//   null,
+//   undefined,
+//   NaN,
+// ];
+
+// console.log(arr[arr.length - 1]); // arrayning oxirgi elementini tanlab oliw // old way
+// console.log(arr.at(-1)); // arrayning oxirgi elementini tanlab oliw // new way
+
+// console.log(arr.toString()); // arrayni stringa o'tkazish
+
+// console.log(typeof arr.join());
