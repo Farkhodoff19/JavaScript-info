9f883f6e184f2b76aeb3f80a0c6f42a295f2a873
Farkhodoff19 <farkhodoffcodes@gmail.com>
2023-02-16 17:15:36 +0300

    new Date()

--- 13-dars/script.js
+++ 13-dars/script.js
@@ -1,15 +1,20 @@
-// DATASTRUCTURE | new Date() | JSON
+// // DATASTRUCTURE | new Date() | JSON
 
-let students = [
-  { id: 1, name: "Farkhodoff", year: 2003 },
-  { id: 2, name: "Gerakloff", year: 2002 },
-];
+// let students = [
+//   { id: 1, name: "Farkhodoff", year: 2003 },
+//   { id: 2, name: "Gerakloff", year: 2002 },
+// ];
 
-// GET CREATE
-let addUser = (data) => {
-  return (students = [...students, data]);
-};
+// // GET CREATE
+// let addUser = (data) => {
+//   return (students = [...students, data]);
+// };
 
-addUser({ name: "Farkhodoff", year: 2007 });
+// addUser({ name: "Farkhodoff", year: 2007 });
 
-console.log(students);
+// console.log(students);
+
+// let date = new Date();
+
+// console.log(date.getDay());
+
